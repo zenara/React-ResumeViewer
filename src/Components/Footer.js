046de70ef884d45076a40bb2742aa4b2cdd0a7fa
Zenara <zenaronline@gmail.com>
2018-03-09 05:00:25 +0530

    Template and Pages with Static Content

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,9 +3,26 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <div>
-        Footer
-      </div>
+        <footer>
+
+        <div class="row">
+  
+           <div class="twelve columns">
+  
+              
+  
+              <ul class="copyright">
+                 <li>&copy; Copyright 2014 CeeVee</li>
+                 <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>   
+              </ul>
+  
+           </div>
+  
+           <div id="go-top"><a class="smoothscroll" title="Back to Top" href="#home"><i class="icon-up-open"></i></a></div>
+  
+        </div>
+  
+     </footer>
     );
   }
 }
